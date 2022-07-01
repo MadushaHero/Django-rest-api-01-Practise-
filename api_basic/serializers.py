d1737b8695bf513733a43d75ffd3c7ed287ac4d2
MadushaHero <madushalakmal@gmail.com>
2022-07-01 10:49:22 +0530

    views

--- api_basic/serializers.py
+++ api_basic/serializers.py
@@ -1,25 +1,18 @@
+
 from rest_framework import serializers
 from .models import Article
 
 
 
-class ArticlesSerializers(serializers.Serializer):
+class ArticlesSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = ['id','title','author']
 
-    title = serializers.CharField(max_length=100)
-    author = serializers.CharField(max_length=50)
-    email = serializers.EmailField(max_length=100)
-    date = serializers.DateField()
+        
 
 
-    def create(self,validated_data):
-       return  Article.objects.create(validated_data)
-
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title',instance.title)
-        instance.author = validated_data.get('author',instance.author)
-        instance.date = validated_data.get('date',instance.date)
-        instance.save()
-        return instance
+    
 
        
         
--- api_basic/views.py
+++ api_basic/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
+from rest_framework.parsers import JSONParser
+from .models import Article
+from .serializers import ArticlesSerializers
 
-# Create your views here.
+
+def article_list(request):
+
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticlesSerializers(articles,many=True)
+        return JsonResponse(serializer.data,safe=False)
+
+    elif request.method == False:
+        data = JSONParser().parse(request)
+        serializer = ArticlesSerializers(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status =201)
+        return JsonResponse(serializer.errors, status =400)
+
+
+
+
+
+
+
+
+
+
+
