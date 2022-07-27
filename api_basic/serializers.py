3977a49b07e0c50c96946088e80bcd58c02bfae3
MadushaHero <madushalakmal@gmail.com>
2022-07-27 13:51:52 +0530

    views and add extra fields for table

--- api_basic/serializers.py
+++ api_basic/serializers.py
@@ -7,7 +7,7 @@ from .models import Article
 class ArticlesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ['id','title','author']
+        fields = ['id','title','author','email']
 
         
 
