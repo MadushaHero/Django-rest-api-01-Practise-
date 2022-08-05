d6461ce12db7354c9034a3b6fd4369014b1d5fdd
MadushaHero <madushalakmal@gmail.com>
2022-08-05 23:51:37 +0530

    Authentication  & Viewsets and routers

--- api_basic/views.py
+++ api_basic/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import AuthenticationError
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 
@@ -11,6 +12,50 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework import generics
 from rest_framework import mixins
+from rest_framework.authentication import SessionAuthentication,TokenAuthentication,BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+
+
+
+class ArticleViewSet(viewsets.ViewSet):
+    def list(self,request):
+        articles = Article.objects.all()
+        serializer = ArticlesSerializer(articles,many=True)
+        return Response(serializer.data)
+    def create(self,request):
+         serializer = ArticlesSerializer(data=request.data)
+
+         if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status =status.HTTP_201_CREATED)
+         return Response(serializer.errors, status =status.HTTP_400_BAD_REQUEST)
+    def retrieve(self,request,pk=None):
+         queryset = Article.objects.all()
+         article = get_object_or_404(queryset,pk=pk)
+         serializer = ArticlesSerializer(article)
+         return Response(serializer.data)
+    
+    def update(self,request,pk=None):
+        
+        article = Article.objects.get(pk=pk)
+
+          
+        serializer = ArticlesSerializer(article,data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status =status.HTTP_400_BAD_REQUEST)
+
+
+   
+
+
+
+
+
 
 
 class GenericAPIView(generics.GenericAPIView,mixins.ListModelMixin,mixins.CreateModelMixin,mixins.UpdateModelMixin,mixins.RetrieveModelMixin,mixins.DestroyModelMixin):
@@ -18,6 +63,10 @@ class GenericAPIView(generics.GenericAPIView,mixins.ListModelMixin,mixins.Create
     queryset = Article.objects.all()
 
     lookup_field = 'id'
+    #authentication_classes = [SessionAuthentication, BasicAuthentication]
+    authentication_classes =[TokenAuthentication]
+
+    permission_classes = [IsAuthenticated]
 
     def get (self,request, id=None):
         if id:
